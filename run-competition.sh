802f17db97a5a848c02b375b8325296d929540bb
P. Michael Furlong <furlong@gmail.com>
2025-07-17 17:31:27 -0400

    Updated run-competition to have the mac address of the muse headbands

--- run-competition.sh
+++ run-competition.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-muselsl stream --address 00:55:DA:BB:A8:44 &
+#muselsl stream --address 00:55:DA:BB:A8:44 &
+# or
+muselsl stream --address 00:55:DA:BB:A6:65 &
 muselsl view & 
 
-python dog-race.py
+python experiment5.py
+
+# then run python dog-race.py <threshold>
 
